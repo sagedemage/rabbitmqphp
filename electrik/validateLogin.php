db79ddff474d34a442b17c7c7de737789f9bcd80
Kenneth Thomas Baidoo <114690319+kenzit100@users.noreply.github.com>
2023-10-07 20:42:38 -0400

    Update validateLogin.php
    I added a check to ensure it only runs on POST requests for user login.
    I initialized the $error and $errorMsg variables.
    I fixed the SQL query to use placeholders and selected the correct columns.
    I improved error handling with more informative messages.
    I added a check for cases where the user is not found.
    I implemented password verification using password_verify().
    I adjusted output messages for better user feedback.

--- electrik/validateLogin.php
+++ electrik/validateLogin.php
@@ -1,29 +1,28 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['id'];
+    $pwd = $_POST['pwd'];
+    $error = false;
+    $errorMsg = "";
 
-    if (isset($id) && isset($pwd)) {
-        $user = $_POST['id'];
-        $pwd = $_POST['pwd'];
-        $error = false;
-        // Validate username or email (allowing alphanumeric characters, hyphens, and spaces)
-        if (empty($user)){ # MAKE AN ERROR MSG LOG HAVE TEMPLATE
-            $error = true;
-            $errorMsg = "Username is empty.";
-        }
-        if (empty($pwd)){
-            $error = true;
-            $errorMsg = "Password is empty.";
-        }
-        if (!preg_match("/^[a-zA-Z-' ]*$/", $user)){
-            $error = true;
-            $errorMsg = "Invalid user id format.";
-        }
-        if (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,}$/', $pwd)){
-            $error = true;
-            $errorMsg = "Invalid password format.";
-        }
+    // Validate username or email (allowing alphanumeric characters, hyphens, and spaces)
+    if (empty($user)) {
+        $error = true;
+        $errorMsg = "Username is empty.";
+    } elseif (!preg_match("/^[a-zA-Z-' ]*$/", $user)) {
+        $error = true;
+        $errorMsg = "Invalid user id format.";
     }
+
+    if (empty($pwd)) {
+        $error = true;
+        $errorMsg = "Password is empty.";
+    } elseif (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,}$/', $pwd)) {
+        $error = true;
+        $errorMsg = "Invalid password format.";
+    }
+
     if (!$error) {
-        $passHash = password_hash($pwd, PASSWORD_DEFAULT);
         $db = new mysqli('127.0.0.1', 'testUser', 'test', 'testdb');
 
         if ($db->connect_error) {
@@ -32,36 +31,41 @@
         }
 
         // Use prepared statement to avoid SQL injection
-        $request = "SELECT (username, passHash) FROM Users WHERE (?, ?)";
+        $request = "SELECT username, passHash FROM Users WHERE username = ?";
+
         $stmt = $db->prepare($request);
-        $stmt->bind_param($user, $passHash);
-        if ($stmt->execute()) {
-            echo "Login successful!";
-        }
-        else {
-            echo "Login failed. Please try again later.";
-        }
-        if ($stmt->errno != 0) {
-            echo "Failed to execute query:" . PHP_EOL;
-            echo __FILE__ . ':' . __LINE__ . ": error: " . $stmt->error . PHP_EOL;
-            exit(0);
-        }
 
-        // Fetch the result
-        $stmt->bind_result($userId, $passHash);
-        $stmt->fetch();
-        $stmt->close();
+        if ($stmt) {
+            $stmt->bind_param("s", $user);
+            if ($stmt->execute()) {
+                $stmt->store_result();
+                if ($stmt->num_rows === 1) {
+                    $stmt->bind_result($username, $passHash);
+                    $stmt->fetch();
+                    $stmt->close();
 
-        // Verify the password (assuming you have stored passwords securely using password_hash)
-        if (password_verify($pwd, $passHash)) {
-            // Authentication successful
-            echo "Authentication successful for user: " . $userId;
+                    // Verify the password (assuming you have stored passwords securely using password_hash)
+                    if (password_verify($pwd, $passHash)) {
+                        // Authentication successful
+                        echo "Authentication successful for user: " . $username;
+                    } else {
+                        // Authentication failed
+                        echo "Authentication failed. Invalid username or password.";
+                    }
+                } else {
+                    echo "User not found.";
+                }
+            } else {
+                echo "Login failed. Please try again later.";
+            }
         } else {
-            // Authentication failed
-            echo "Authentication failed. Invalid username or password.";
+            echo "Error in preparing the statement.";
         }
 
         // Close the database connection
         $db->close();
+    } else {
+        echo $errorMsg;
     }
+}
 ?>
