d7b2b85f837bb43e4763d5e0c78d6ab0a5d204b4
Salmaan Saeed <salsaeed999@gmail.com>
2023-10-11 12:35:46 -0400

    fix install script

--- install_web_server_to_apache.sh
+++ install_web_server_to_apache.sh
@@ -2,3 +2,5 @@
 sudo mkdir -v /var/www/electrik
 sudo cp -rv electrik/*.html /var/www/electrik/
 sudo cp -rv electrik/*.php /var/www/electrik/
+sudo cp -rv electrik/*.css /var/www/electrik/
+sudo cp -rv electrik/*.js /var/www/electrik/
