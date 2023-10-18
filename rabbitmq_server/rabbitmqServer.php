e3b7c4718acd0106206b0d0ff727e35a2787d339
Salmaan Saeed <salsaeed999@gmail.com>
2023-10-18 12:08:23 -0400

    push broken changes for queuing system for user register

--- rabbitmq_server/rabbitmqServer.php
+++ rabbitmq_server/rabbitmqServer.php
@@ -53,6 +53,57 @@ function doLogin($username, $password) {
 	}
 }
 
+function doRegister($email, $username, $password) {
+	/* Register a User */
+	$passHash = password_hash($password, PASSWORD_DEFAULT);
+	$host = "localhost";
+	$db_user = "admin";
+	$db_pass = "adminPass";
+	$db_name = "ProjectDB";
+	$db = new mysqli($host, $db_user, $db_pass, $db_name);
+
+	if ($db->connect_errno != 0) {
+		echo "Failed to connect to the database: " . $db->connect_error;
+		$db->close();
+		exit(0);
+	}
+
+	// Username exist in database
+	$request = "select * from Users where username = '$username'";
+	$result = mysqli_query($db, $request);
+	$count = mysqli_num_rows($result);
+
+	if ($count > 0) {
+		return "Username is already taken";
+	}
+
+	// Email exist in database
+	$request = "select * from Users where email = '$email'";
+	$result = mysqli_query($db, $request);
+	$count = mysqli_num_rows($result);
+
+	if ($count > 0) {
+		return "Email is already taken";
+	}
+
+	$request = "INSERT INTO Users (username, email, passHash) VALUES (?, ?, ?)";
+	$stmt = $db->prepare($request);
+	$stmt->bind_param("sss", $username, $email, $passHash);
+
+	if ($stmt->execute()) {
+		// Redirect the user to the home page after successful registration
+		//header("Location: home.html");
+		$stmt->close();
+		$db->close();
+		return "Registration success.";
+	} 
+
+	else {
+		$db->close();
+		return "Registration failed. Please try again later.";
+	}
+} 
+
 function requestProcessor($request) {
 	echo "received request".PHP_EOL;
 	var_dump($request);
@@ -63,6 +114,8 @@ function requestProcessor($request) {
 	{
 	case "Login":
 		return doLogin($request['username'], $request['password']);
+	case "Register":
+		return doRegister($request['email'], $request['username'], $request['password']);
 	}
 	return array("returnCode" => '0', 'message'=>"server received request and processed");
 }
