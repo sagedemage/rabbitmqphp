e1a42effa8ac109192ef0644f5f99a8d90b78c04
Kenneth Thomas Baidoo <114690319+kenzit100@users.noreply.github.com>
2023-10-26 17:57:10 -0400

    Update register.php

--- electrik/register.php
+++ electrik/register.php
@@ -6,65 +6,50 @@ require_once('../rabbitmq_lib/get_host_info.inc');
 require_once('../rabbitmq_lib/rabbitMQLib.inc');
 
 /* Client */
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirm'])) {
-	$user = $_POST['id'];
-	$email = $_POST['email'];
-	$pwd = $_POST['pwd'];
-	$cfrmpwd = $_POST['cfrmpwd'];
-	$error = false;
-	$errorMsgs = array();
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+    $user = $_POST['id'];
+    $pwd = $_POST['pwd'];
+    $error = false;
+    $errorMsgs = array();
 
-	// Validation
-	if (isset($user) && isset($pwd) && isset($email)) {
+    // Validation and sanitization code here...
 
-		if (empty($user) || !isset($user)) { # MAKE AN ERROR MSG LOG HAVE TEMPLATE
-			$error = true;
-			$errorMsgs[] = "Username is empty.";
-		} else {
-			$user = htmlspecialchars($user, ENT_QUOTES, 'UTF-8'); # SANITIZE
-		}
-		if (empty($email) || !isset($email)) {
-			$error = true;
-			$errorMsgs[] = "Email is empty.";
-		} else {
-			$email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
-		}
-		if (empty($pwd) || !isset($pwd)) {
-			$error = true;
-			$errorMsgs[] = "Password is empty.";
-		}
-		if (empty($cfrmpwd) || !isset($cfrmpwd)) {
-			$error = true;
-			$errorMsgs[] = "Confirmation Password is empty.";
-		}
-		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			$error = true;
-			$errorMsgs[] = "Invalid email format.";
-		}
-		if ($pwd != $cfrmpwd) {
-			$error = true;
-			$errorMsgs[] = "Passwords do not match.";
-		}
-	}
-	if (!$error) {
-		$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-
-		/* Send register request to server */
-		$request = array();
-		$request['type'] = "Register";
-		$request['email'] = $email;
-		$request['username'] = $user;
-		$request['password'] = $pwd;
-		$response = $client->send_request($request);
-
-		echo $response;
-		
+    if (!isset($user) || empty($user)) {
+        $error = true;
+        $errorMsgs[] = "Username is empty.";
+    } 
+    else {
+        $user = htmlspecialchars($user, ENT_QUOTES, 'UTF-8'); 
     }
-	else if ($error) {
-		foreach ($errorMsgs as $error) {
-			echo $error . '<br>';
-			error_log($error, 3, "error.log");
-		}
-	}
+
+    if (!isset($pwd) || empty($pwd)) {
+        $error = true;
+        $errorMsgs[] = "Password is empty.";
+    }
+
+    if (!$error) {
+        $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+        /* Send login request to server */
+        $request = array();
+        $request['type'] = "Login";
+        $request['username'] = $user;
+        $request['password'] = $pwd;
+        $response = $client->send_request($request);
+
+        // Check if the login response is successful, and then set a session cookie
+        if ($response === "success") {
+            // Set a session cookie to persist authentication
+            setcookie('user_id', $user, time() + 3600, '/');
+        } else {
+            // Display a popup message for invalid username or password
+            echo '<script>alert("Invalid Username or Password. Please Try Again.");</script>';
+            echo '<script>window.location.href = "./login.html";</script>';
+        }
+    } else if ($error) {
+        foreach ($errorMsgs as $error) {
+            echo $error . '<br>';
+            error_log($error, 3, "error.log");
+        }
+    }
 }
 ?>
