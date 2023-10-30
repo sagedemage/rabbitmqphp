da3e98fc4b81d1c1f2be8441e875ce483d76aa51
Kenneth <kenzit100@gmail.com>
2023-10-29 22:11:52 -0400

    Updated the logout.php

--- electrik/logout.php
+++ electrik/logout.php
@@ -12,10 +12,11 @@ if (isset($_SESSION['username'])) {
 
     //Clear the session cookie
     $name = "user_id";
-    setcookie($name, "", time() - 3600, "/");
+    $expires_or_options = time() - 3600;
+    setcookie($name, "", $expires_or_options, "/");
 
 
-     //Redirect the user to the homepafe after logging out
+     //Redirect the user to the homepage after logging out
      header("Location: login.html");
      exit;
 
