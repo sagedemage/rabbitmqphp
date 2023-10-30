1e44928ee452493a7466a1aa51e107856c55619f
Kenneth <kenzit100@gmail.com>
2023-10-29 22:35:27 -0400

    Updated the home.html

--- electrik/logout.php
+++ electrik/logout.php
@@ -1,14 +1,4 @@
 <?php
-//start or resume the session
-session_start();
-
-//check if the user is logged in (a session variable is set)
-if (isset($_SESSION['username'])) {
-    //unset all of the session variables
-    $_SESSION = array();
-
-    //Destroy the session
-    session_destroy();
 
     //Clear the session cookie
     $name = "user_id";
@@ -19,7 +9,5 @@ if (isset($_SESSION['username'])) {
      //Redirect the user to the homepage after logging out
      header("Location: login.html");
      exit;
-
-}
 
 ?>
