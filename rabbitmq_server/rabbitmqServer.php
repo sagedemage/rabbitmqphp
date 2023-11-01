2dd65676162de36d0b8be8f75578f0127545c669
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-01 14:38:46 -0400

    change imports

--- rabbitmq_server/rabbitmqServer.php
+++ rabbitmq_server/rabbitmqServer.php
@@ -2,9 +2,9 @@
 <?php
 ini_set('display_errors', 1);
 
-require_once('../rabbitmq_lib/path.inc');
-require_once('../rabbitmq_lib/get_host_info.inc');
-require_once('../rabbitmq_lib/rabbitMQLib.inc');
+require_once('rabbitmq_lib/path.inc');
+require_once('rabbitmq_lib/get_host_info.inc');
+require_once('rabbitmq_lib/rabbitMQLib.inc');
 
 function requestProcessor($request) {
 	// send request to receiver
