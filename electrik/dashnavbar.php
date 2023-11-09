42041b966678d0c696a693adde0c475453e26e2a
Kenneth <kenzit100@gmail.com>
2023-11-08 20:50:29 -0500

    Added dashnavbar.php for the dasboard

---
+++ electrik/dashnavbar.php
@@ -0,0 +1,15 @@
+<nav class="dashboard-navbar">
+    <a href="index.php">HOME</a>
+    <a href="aboutus.php">ABOUT US</a>
+    <div class="right">
+        <div class="dropdown">
+            <button class="dropbtn">ACCOUNT</button>
+            <div class="dropdown-content">
+                <a href="dashbaord.html">DASHBOARD</a>
+                <a href="forgotpassword.html">RESET PASSWORD</a>
+                <a href="logout.php">LOGOUT</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
