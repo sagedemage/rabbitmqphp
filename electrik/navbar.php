24bbb0711c40bed2a099e00d5773208de68007e6
Kenneth <kenzit100@gmail.com>
2023-11-08 20:20:06 -0500

    Updated navbar.php

--- electrik/navbar.php
+++ electrik/navbar.php
@@ -1,8 +1,8 @@
 <nav>
-<a href="index.php">HOME</a>
+  <a href="index.php">HOME</a>
   <a href="aboutus.php">ABOUT US</a>
   <div class="right">
-        <a href="register.html" class="right">REGISTER</a>
-        <a href="login.html" class="right">LOGIN</a>
+    <a href="register.html">REGISTER</a>
+    <a href="login.html">LOGIN</a>
   </div>
 </nav>
