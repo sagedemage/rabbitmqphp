71e5a7f0c2b89ef9c1d9b47463e8916e138066f2
Kenneth <kenzit100@gmail.com>
2023-11-09 21:44:39 -0500

    Updated the navabar.php

--- electrik/navbar.php
+++ electrik/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="index.php">HOME</a>
     <a class="navbar-brand" href="aboutus.php">ABOUT US</a>
-    <div class="ml-auto">
+    <div class="ms-auto">
         <a class="navbar-brand" href="register.html">REGISTER</a>
         <a class="navbar-brand" href="login.html">LOGIN</a>
     </div>
