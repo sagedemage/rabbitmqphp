f1268bb7556ba4df87c0e6f8426a54307e7c2f6a
Kenneth <kenzit100@gmail.com>
2023-11-09 21:55:19 -0500

    Updated the login.php

--- electrik/login.php
+++ electrik/login.php
@@ -6,12 +6,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-lhQfs5gIvKGp8r+8Em+RmA8w2/3cGLCnUc9/7IeDJdzmfa/iRs2t5u8Zn6QAr4p" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/js/bootstrap.min.js" integrity="sha384-e3r0CxPrMZl2UcKc6alAt/koyToT5FSAu4b7/WFg3IkKBAfxyFsXcAnO9DDCpHEhL" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/js/bootstrap.min.js" integrity="sha384-e3r0CxPrMZl2UcKc6lAt/koyToT5FSAu4b7/WFg3IkKBAfxyFsXcAnO9DDCpHEhL" crossorigin="anonymous"></script>
 
   <style>
     body {
       font-family: Arial, Helvetica, sans-serif;
       background-color: rgba(0, 47, 255, 0.61);
+      align-items: center;
+      justify-content: center;
     }
 
     form {
@@ -61,6 +63,33 @@
       float: right;
       padding-top: 16px;
     }
+
+    /* Add styles for the custom navbar */
+    nav {
+      background-color: #343a40; /* Set the background color */
+      padding: 10px; /* Add padding to the navbar */
+      margin-bottom: 20px; /* Add margin to separate from the content */
+    }
+
+    .navbar-brand {
+      color: #ffffff !important; /* Set the text color to white */
+      margin-right: 20px; /* Add margin to the right of each navbar item */
+    }
+
+    .ms-auto {
+      margin-left: auto !important;
+    }
+
+    @media screen and (max-width: 300px) {
+      span.psw {
+        display: block;
+        float: none;
+      }
+
+      .cancelbtn {
+        width: 100%;
+      }
+    }
   </style>
 </head>
 
@@ -68,10 +97,13 @@
 
   <?php include('navbar.php'); ?>
 
-  <div class="container">
-    <h2 style="text-align: center;">LOGIN</h2>
+  <div id="header"></div>
+  
 
-    <form action="/validateLogin.php" method="post">
+  <h2 style="text-align: center;">LOGIN</h2>
+
+  <form action="/validateLogin.php" method="post">
+    <div class="container">
       <label for="id"><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="id" required>
 
@@ -82,12 +114,14 @@
       <label>
         <input type="checkbox" checked="checked" name="remember"> Remember me
       </label>
+    </div>
 
+    <div class="container" style="background-color:#f1f1f1">
       <button type="button" class="cancelbtn"><a href="./index.php">Back</a></button>
       <span class="psw"><a href="./forgotpassword.html">Forgot password?</a></span>
       <span class="register">Don't have an account? <a href="./register.html">Sign Up</a></span>
-    </form>
-  </div>
+    </div>
+  </form>
 
   <?php include('footer.php'); ?>
 
