4f3b7669d2d921eba5757f6077a728de27b68b18
Kenneth <kenzit100@gmail.com>
2023-11-09 21:14:13 -0500

    Updated the login.php

--- electrik/login.php
+++ electrik/login.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <style>
     body {
       font-family: Arial, Helvetica, sans-serif;
-      
+      background-color: rgba(0, 47, 255, 0.61);
       align-items: center;
       justify-content: center;
+      margin: 0; /* Ensure no default margin */
     }
 
     form {
       border: 3px solid #f1f1f1;
-      width: 50%; /* Adjust the width as needed */
-      margin: 0 auto; /* Center the form horizontally */
-      margin-top: 50px; /* Add margin at the top */
+      width: 50%;
+      margin: 0 auto;
+      margin-top: 50px;
+      background-color: #ffffff; /* Add a background color to the form */
+      padding: 20px; /* Add padding to the form */
     }
 
-    input[type=text], input[type=password] {
+    input[type=text],
+    input[type=password] {
       width: 100%;
       padding: 12px 20px;
       margin: 8px 0;
@@ -61,45 +66,49 @@
         display: block;
         float: none;
       }
+
       .cancelbtn {
         width: 100%;
       }
     }
   </style>
 </head>
+
 <body>
 
-<?php include('navbar.php'); ?>
+  <?php include('navbar.php'); ?>
 
-<div id="header"></div>
-<div id="navbar"></div>
+  <div id="header"></div>
+  <div id="navbar"></div>
 
-<h2 style="text-align: center;">LOGIN</h2>
+  <h2 style="text-align: center;">LOGIN</h2>
 
-<form action="/validateLogin.php" method="post">
-  <div class="container">
-    <label for="id"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="id" required>
+  <form action="/validateLogin.php" method="post">
+    <div class="container">
+      <label for="id"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="id" required>
 
-    <label for="pwd"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="pwd" required>
-        
-    <button type="submit" name="login">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
+      <label for="pwd"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="pwd" required>
 
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn"><a href="./index.php">Back</a></button>
-    <span class="psw"><a href="./forgotpassword.html">Forgot password?</a></span>
-    <span class="register">Don't have an account? <a href="./register.html">Sign Up</a></span>
-  </div>
-</form>
+      <button type="submit" name="login">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
 
-<?php include('footer.php'); ?>
+    <div class="container">
+      <button type="button" class="cancelbtn"><a href="./index.php">Back</a></button>
+      <span class="psw"><a href="./forgotpassword.html">Forgot password?</a></span>
+      <span class="register">Don't have an account? <a href="./register.html">Sign Up</a></span>
+    </div>
+  </form>
+
+  <?php include('footer.php'); ?>
 
 </body>
+
 </html>
+
 
 
