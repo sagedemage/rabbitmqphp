2c50c6e16503e8b180bde30ad6f09a1d5bcd8081
Kenneth <kenzit100@gmail.com>
2023-11-09 20:08:40 -0500

    Updated index.php

--- electrik/index.php
+++ electrik/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Page Title</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
+
 
 <?php include('navbar.php'); ?>
 
