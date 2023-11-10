8f3f1d9ddc22775cc28288b552c140f9ae2374f8
Kenneth <kenzit100@gmail.com>
2023-11-09 21:25:09 -0500

    Updated the login.php

--- electrik/login.php
+++ electrik/login.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha384-oVf3yoD1Q81a1PyRZXf+VdCmbso9g2U5MWZtUwnlfxI5cUJVOFl5WKIbShc6BbQ" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-lhQfs5gIvKGp8r+8Em+RmA8w2/3cGLCnUc9/7IeDJdzmfa/iRs2t5u8Zn6QAr4p" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/js/bootstrap.min.js" integrity="sha384-e3r0CxPrMZl2UcKc6lAt/koyToT5FSAu4b7/WFg3IkKBAfxyFsXcAnO9DDCpHEhL" crossorigin="anonymous"></script>
 
@@ -12,16 +12,15 @@
     body {
       font-family: Arial, Helvetica, sans-serif;
       background-color: rgba(0, 47, 255, 0.61);
-      display: flex;
       align-items: center;
       justify-content: center;
-      margin: 0;
-      height: 100vh;
     }
 
     form {
       border: 3px solid #f1f1f1;
       width: 50%;
+      margin: 0 auto;
+      margin-top: 50px;
       background-color: #ffffff;
       padding: 20px;
     }
@@ -82,10 +81,13 @@
 
   <?php include('navbar.php'); ?>
 
-  <div class="container">
-    <h2 style="text-align: center;">LOGIN</h2>
+  <div id="header"></div>
+  <div id="navbar"></div>
 
-    <form action="/validateLogin.php" method="post">
+  <h2 style="text-align: center;">LOGIN</h2>
+
+  <form action="/validateLogin.php" method="post">
+    <div class="container">
       <label for="id"><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="id" required>
 
@@ -96,12 +98,14 @@
       <label>
         <input type="checkbox" checked="checked" name="remember"> Remember me
       </label>
+    </div>
 
+    <div class="container" style="background-color:#f1f1f1">
       <button type="button" class="cancelbtn"><a href="./index.php">Back</a></button>
       <span class="psw"><a href="./forgotpassword.html">Forgot password?</a></span>
       <span class="register">Don't have an account? <a href="./register.html">Sign Up</a></span>
-    </form>
-  </div>
+    </div>
+  </form>
 
   <?php include('footer.php'); ?>
 
