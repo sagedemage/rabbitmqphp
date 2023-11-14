efa7cfa4fd9952dfe1966edc20c939be267105c3
Kenneth <kenzit100@gmail.com>
2023-11-14 17:06:33 -0500

    Updated registerAcct.php

--- electrik/registerAcct.php
+++ electrik/registerAcct.php
@@ -30,37 +30,40 @@
     <?php include('navbar.php'); ?>
 
 	<h2 style="text-align: center;">REGISTER</h2>
+	<div class="form-container">
+    <form action="/register.php" method="post" accept-charset="utf-8">
+        <!-- Route to validateLogin.php for checking -->
 
-    <div class="form-container">
-        <form action="/register.php" method="post" accept-charset="utf-8">
-            <!-- Route to validateLogin.php for checking -->
+        <div class="container form-group">
+            <label for="id"><b>Username</b></label>
+            <input id="id" type="text" placeholder="Enter Username" name="id" required>
+        </div>
 
-            <div class="container form-group">
-                <label for="id"><b>Username</b></label>
-                <input id="id" type="text" placeholder="Enter Username" name="id" required>
-            </div>
+        <div class="container form-group">
+            <label for="email"><b>Email</b></label>
+            <input id="email" type="text" placeholder="Enter Email" name="email" required>
+        </div>
 
-            <div class="container form-group">
-                <label for="email"><b>Email</b></label>
-                <input id="email" type="text" placeholder="Enter Email" name="email" required>
-            </div>
+        <div class="container form-group">
+            <label for="pwd"><b>Password</b></label>
+            <input id="pwd" type="password" placeholder="Enter Password" name="pwd" required>
+        </div>
 
-            <div class="container form-group">
-                <label for="pwd"><b>Password</b></label>
-                <input id="pwd" type="password" placeholder="Enter Password" name="pwd" required>
-            </div>
+        <div class="container form-group">
+            <label for="cfrmpwd"><b>Confirm Password</b></label>
+            <input id="cfrmpwd" type="password" placeholder="Confirm Password" name="cfrmpwd" required>
+        </div>
 
-            <div class="container form-group">
-                <label for="cfrmpwd"><b>Confirm Password</b></label>
-                <input id="cfrmpwd" type="password" placeholder="Confirm Password" name="cfrmpwd" required>
-            </div>
+        <div class="container">
+            <button type="submit" name="confirm">Submit</button>
+            <button type="button" class="caacncelbtn" onclick="window.location.href='./index.php'">Cancel</button>
+        </div>
+    </form>
+</div>
 
-            <div class="container">
-                <button type="submit" name="confirm">Submit</button>
-                <button type="button" class="cancelbtn" onclick="window.location.href='./index.php'">Cancel</button>
-            </div>
-        </form>
-    </div>
+
+
+
 
     <?php include('footer.php'); ?>
 </body>
