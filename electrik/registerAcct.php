a69e1164f05ec30d35cea7fcfc94b30ae58d7f66
Kenneth <kenzit100@gmail.com>
2023-11-14 17:17:01 -0500

    Updated registerAcct.php

--- electrik/registerAcct.php
+++ electrik/registerAcct.php
@@ -2,70 +2,136 @@
 <html lang="en">
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
-    <style>
-        /* Additional styles for centering the form */
-        .form-container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh; /* Adjust as needed */
-        }
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+    integrity="sha384-lhQfs5gIvKGp8r+8Em+RmA8w2/3cGLCnUc9/7IeDJdzmfa/iRs2t5u8Zn6QAr4p" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/js/bootstrap.min.js"
+    integrity="sha384-e3r0CxPrMZl2UcKc6lAt/koyToT5FSAu4b7/WFg3IkKBAfxyFsXcAnO9DDCpHEhL"
+    crossorigin="anonymous"></script>
 
-        /* Adjusted styles for better structure */
-        .form-group {
-            margin-bottom: 20px;
-        }
+  <style>
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+      background-color: rgba(0, 47, 255, 0.61);
+      align-items: center;
+      justify-content: center;
+      margin: 0;
+      padding: 0;
+      height: 100vh;
+    }
 
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-    </style>
+    form {
+      border: 3px solid #f1f1f1;
+      width: 50%;
+      margin: 0 auto;
+      margin-top: 50px;
+      background-color: #ffffff;
+      padding: 20px;
+    }
+
+    input[type=text],
+    input[type=password] {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+    }
+
+    button {
+      background-color: #04AA6D;
+      color: white;
+      padding: 14px 20px;
+      margin: 8px 0;
+      border: none;
+      cursor: pointer;
+      width: 100%;
+    }
+
+    button:hover {
+      opacity: 0.8;
+    }
+
+    .cancelbtn {
+      width: auto;
+      padding: 10px 18px;
+      background-color: #f44336;
+    }
+
+    .container {
+      padding: 16px;
+    }
+
+    span.psw {
+      float: right;
+      padding-top: 16px;
+    }
+
+    /* Add styles for the custom navbar */
+    nav {
+      background-color: #343a40; /* Set the background color */
+      padding: 10px; /* Add padding to the navbar */
+      margin-bottom: 20px; /* Add margin to separate from the content */
+    }
+
+    .navbar-brand {
+      color: #ffffff !important; /* Set the text color to white */
+      margin-right: 20px; /* Add margin to the right of each navbar item */
+    }
+
+    .ms-auto {
+      margin-left: auto !important;
+    }
+
+    @media screen and (max-width: 300px) {
+      span.psw {
+        display: block;
+        float: none;
+      }
+
+      .cancelbtn {
+        width: 100%;
+      }
+    }
+  </style>
 </head>
 
 <body>
 
-    <?php include('navbar.php'); ?>
+  <?php include('navbar.php'); ?>
 
-	<h2 style="text-align: center;">REGISTER</h2>
-	<div class="form-container">
-    <form action="/register.php" method="post" accept-charset="utf-8">
-        <!-- Route to validateLogin.php for checking -->
+  <div id="header"></div>
 
-        <div class="container form-group">
-            <label for="id"><b>Username</b></label>
-            <input id="id" type="text" placeholder="Enter Username" name="id" required>
-        </div>
+  <h2 style="text-align: center;">REGISTER</h2>
 
-        <div class="container form-group">
-            <label for="email"><b>Email</b></label>
-            <input id="email" type="text" placeholder="Enter Email" name="email" required>
-        </div>
+  <form action="/validateRegister.php" method="post">
+    <div class="container">
+      <label for="id"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="id" required>
 
-        <div class="container form-group">
-            <label for="pwd"><b>Password</b></label>
-            <input id="pwd" type="password" placeholder="Enter Password" name="pwd" required>
-        </div>
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" required>
 
-        <div class="container form-group">
-            <label for="cfrmpwd"><b>Confirm Password</b></label>
-            <input id="cfrmpwd" type="password" placeholder="Confirm Password" name="cfrmpwd" required>
-        </div>
+      <label for="pwd"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="pwd" required>
 
-        <div class="container">
-            <button type="submit" name="confirm">Submit</button>
-            <button type="button" class="cancelbtn" onclick="window.location.href='./index.php'">Cancel</button>
-        </div>
-    </form>
-</div>
+      <label for="cfrmpwd"><b>Confirm Password</b></label>
+      <input type="password" placeholder="Confirm Password" name="cfrmpwd" required>
 
+      <button type="submit" name="register">Register</button>
+    </div>
 
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" class="cancelbtn"><a href="./index.php">Back</a></button>
+    </div>
+  </form>
 
+  <?php include('footer.php'); ?>
 
-
-    <?php include('footer.php'); ?>
 </body>
 
 </html>
