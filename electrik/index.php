8959ab6b15f8cd063a6e5e536ad3400af2b7a57c
Kenneth <kenzit100@gmail.com>
2023-11-14 15:57:30 -0500

    Updated index.php

--- electrik/index.php
+++ electrik/index.php
@@ -54,6 +54,15 @@ if (isset($_SESSION['USER_ID'])) {
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
+
+
+<script>
+  $(document).ready(function () {
+    $('#carouselExample').carousel();
+  });
+</script>
+
+
 </body>
 </html>
 
