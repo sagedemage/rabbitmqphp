c3c3fbe86d781f53e91e26618c4b88d7d1bc63b1
Kenneth <kenzit100@gmail.com>
2023-11-14 21:47:17 -0500

    Updated footer.php

--- electrik/footer.php
+++ electrik/footer.php
@@ -1,5 +1,26 @@
+<!-- footer.php -->
+
 
 <footer>
-    &copy; 2023 Electrik.com. All rights reserved. <a href="terms.html">Terms of Service</a>
+    &copy; 2023 Electrik.com. All rights reserved. <a class="terms-link" href="terms.html">Terms of Service</a>
 </footer>
+
+
+<style>
+    /* Style the Terms of Service link in the footer */
+    .terms-link {
+        color: white;
+        text-decoration: none; /* Remove underline */
+    }
+
+
+    /* Hover effect for the Terms of Service link */
+    .terms-link:hover {
+        text-decoration: underline;
+    }
+</style>
+
+
+
+
 
