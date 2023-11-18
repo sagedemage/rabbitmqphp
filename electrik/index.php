ff51778985bf183376bcbc6df9ea5bb2010f14e1
Kenneth <kenzit100@gmail.com>
2023-11-18 18:44:20 -0500

    Updated index.php

--- electrik/index.php
+++ electrik/index.php
@@ -25,26 +25,7 @@
 
 
 
-<!-- Slide Deck -->
-<div id="carouselExample" class="carousel slide d-flex justify-content-center" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="https://via.placeholder.com/1200x200" class="d-block w-100" alt="Slide 1">
-        </div>
-        <div class="carousel-item">
-            <img src="https://via.placeholder.com/1200x200" class="d-block w-100" alt="Slide 2">
-        </div>
-        <!-- Add more slides as needed -->
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
+
 
 <?php
 session_start();
