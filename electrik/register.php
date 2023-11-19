6d18fd4e322065f14b7eb61e78cbbc92396d24d6
Kenneth <kenzit100@gmail.com>
2023-11-18 21:32:47 -0500

    Updated register.php

---
+++ electrik/register.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register Account</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css"> <!-- Link to your CSS stylesheet for additional styling -->
+</head>
+<body>
+
+<!-- Include the common header and navbar -->
+<?php include('navbar.php'); ?>
+
+<div class="container mt-5">
+    <h2>Register Account</h2>
+
+    <form action="/user_register.php" method="post" accept-charset="utf-8">
+        <div class="mb-3">
+            <label for="id" class="form-label"><b>Username</b></label>
+            <input id="id" type="text" class="form-control" placeholder="Enter Username" name="id" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="email" class="form-label"><b>Email</b></label>
+            <input id="email" type="text" class="form-control" placeholder="Enter Email" name="email" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="pwd" class="form-label"><b>Password</b></label>
+            <input id="pwd" type="password" class="form-control" placeholder="Enter Password" name="pwd" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="cfrmpwd" class="form-label"><b>Confirm Password</b></label>
+            <input id="cfrmpwd" type="password" class="form-control" placeholder="Confirm Password" name="cfrmpwd" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary" name="confirm">Submit</button>
+    </form>
+
+    <div class="mt-3">
+        <a class="btn btn-secondary" href="./home.html">Cancel</a>
+    </div>
+</div>
+
+<!-- Include the common footer -->
+<?php include('footer.php'); ?>
+
+<!-- Bootstrap JS (optional, but required for some features) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+
