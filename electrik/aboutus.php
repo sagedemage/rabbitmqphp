13e1173018ccc973f3ee0dc86e6915a7988b7dd8
Kenneth <kenzit100@gmail.com>
2023-11-18 21:25:47 -0500

    Updated aboutus.php

---
+++ electrik/aboutus.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="style.css"> <!-- Link to your CSS stylesheet for styling -->
+</head>
+<body>
+
+<!-- Include the common header and navbar -->
+<?php include('navbar.php'); ?>
+
+<main>
+    <section>
+        <h2>Our Company</h2>
+        <p>
+            Insert a brief description of your company here. You can talk about your mission, vision, and what sets your company apart.
+        </p>
+    </section>
+
+    <section>
+        <h2>Our Team</h2>
+        <p>
+            Introduce your team members and their roles. Highlight their expertise and contributions to the company's success.
+        </p>
+    </section>
+
+    <section>
+        <h2>Our History</h2>
+        <p>
+            Provide a timeline of your company's history. Mention key milestones, achievements, and important events.
+        </p>
+    </section>
+</main>
+
+<!-- Include the common footer -->
+<?php include('footer.php'); ?>
+
+</body>
+</html>
+
+
