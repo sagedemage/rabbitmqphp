0c2dcb50913458966f4ec9f98a8dfb1feed9fbc7
Kenneth <kenzit100@gmail.com>
2023-11-18 23:14:09 -0500

    Updated register.php

--- electrik/register.php
+++ electrik/register.php
@@ -45,6 +45,11 @@
 
 <!-- Include the common footer -->
 <?php include('footer.php'); ?>
+<div class="footer">
+    &copy; 2023 Electrik.com. All rights reserved. <a class="terms-link" href="terms.php">Terms of Service</a>
+</div>
+
+
 
 <!-- Bootstrap JS (optional, but required for some features) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
