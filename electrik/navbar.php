520551506e0f1ac95484853c8ec93d9ffc37103e
Kenneth <kenzit100@gmail.com>
2023-11-18 20:25:28 -0500

    Added navabr.php

---
+++ electrik/navbar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    /* Add your CSS styling for the navbar here */
+    .navbar {
+      overflow: hidden;
+      background-color: #333;
+    }
+
+    .navbar a {
+      float: left;
+      display: block;
+      color: #f2f2f2;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+    }
+
+    .navbar a:hover {
+      background-color: #ddd;
+      color: black;
+    }
+
+    .navbar .right {
+      float: right;
+    }
+  </style>
+</head>
+<body>
+
+<div class="navbar">
+
+  <a href="index.html">HOME</a>
+  <a href="aboutus.html">ABOUT US</a>
+  <a href="register.html" class="right">REGISTER</a>
+  <a href="login.html" class="right">LOGIN</a>
+ 
+ 
+</div>
+
+</body>
+</html>
+
+
+
