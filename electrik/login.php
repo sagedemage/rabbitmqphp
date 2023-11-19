0b3a1d486810dbe9ae004f2ffca0c4d77a3eed94
Kenneth <kenzit100@gmail.com>
2023-11-19 18:11:59 -0500

    Updated login.php

--- electrik/login.php
+++ electrik/login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
