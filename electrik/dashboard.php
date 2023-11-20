45c18923a0a6f7589fc7c98bd0f46a7a0982d810
Kenneth <kenzit100@gmail.com>
2023-11-19 19:08:13 -0500

    Updated dashboard.php

--- electrik/dashboard.php
+++ electrik/dashboard.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css"> <!-- Link to your CSS stylesheet for additional styling -->
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="style.css"> <!-- Link to your CSS stylesheet for additional styling -->
 </head>
 
 <body>
