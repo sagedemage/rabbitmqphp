7ac2d287b3b2d2cfeaaa2b479bd2390defc910f1
Kenneth <kenzit100@gmail.com>
2023-11-19 19:59:12 -0500

    Updated dashnav.php

--- electrik/dashnav.php
+++ electrik/dashnav.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-custom">
    <div class="container-fluid">
        <a class="navbar-brand" href="index.php">Electrik</a>
        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
