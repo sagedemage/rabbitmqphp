c1721fa36d03e8a055e05f34abf5b3df5c2369b6
Kenneth <kenzit100@gmail.com>
2023-11-19 20:59:36 -0500

    Updated index.php

--- electrik/index.php
+++ electrik/index.php
@@ -25,28 +25,6 @@ if (isset($_SESSION['USER_ID'])) {
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <title>Page Title</title>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <!-- Bootstrap CSS -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
-   <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-<?php include('navbar.php'); ?>
-
-<?php
-session_start();
-if (isset($_SESSION['USER_ID'])) {
-   echo "You are logged in as: " . $_SESSION['user_id'];
-} else {
-   echo "You are not logged in.";
-}
-?>
 
 <!-- Bootstrap Carousel -->
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
