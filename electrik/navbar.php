e5ae0ce5a15e6988b24b6da9561643f2b88fe64c
Kenneth <kenzit100@gmail.com>
2023-11-19 20:26:08 -0500

    Updated navbar.php

--- electrik/navbar.php
+++ electrik/navbar.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-custom">
+<nav class="navbar navbar-expand-lg navbar-dark bg-custom">
   <div class="container-fluid">
       <a class="navbar-brand" href="index.php">Electrik</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
