c142605a4ad285fec78faf1f6f71c3713bfd724d
Kenneth <kenzit100@gmail.com>
2023-11-19 21:29:55 -0500

    Updated dashnav.php

--- electrik/dashnav.php
+++ electrik/dashnav.php
@@ -50,6 +50,10 @@
 
 <!-- Bootstrap JS (optional, but required for some features) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+
+
 </body>
 </html>
 
