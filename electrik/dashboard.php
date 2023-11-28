0f31eca1dfc8e08f2158c6008efadafc05e13e5d
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-27 20:25:24 -0500

    moved js code in dashboard.php into dashboard.js

--- electrik/dashboard.php
+++ electrik/dashboard.php
@@ -145,28 +145,7 @@
 
 <!-- Your existing script for user session verification -->
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-<script>
-   function get_cookie_value(cookie_name) {
-       let decodedCookie = decodeURIComponent(document.cookie);
-       const cookieValue = decodedCookie
-           .split("; ")
-           .find((row) => row.startsWith(cookie_name + "="))
-           ?.split("=")[1];
-       return cookieValue;
-   }
-   let user_id = get_cookie_value("user_id");
-   axios.post('/verify_user_session.php', {
-       user_id: user_id,
-   })
-       .then(function(response) {
-           if (response.data !== true) {
-               location.href = "/login.php";
-           }
-       })
-       .catch(function(error) {
-           console.log(error);
-       });
-</script>
+<script src="dashboard.js"></script>
 
 </body>
 </html>
