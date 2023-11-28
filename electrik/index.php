85a1d3943b74a859437eedcb71b34d028977242c
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-28 00:35:02 -0500

    made height for slide images larger

--- electrik/index.php
+++ electrik/index.php
@@ -27,21 +27,21 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="images/image6.jpg" class="d-block w-100" alt="Slide 1" style="height: 18rem;">
+      <img src="images/image6.jpg" class="d-block w-100" alt="Slide 1" style="height: 25rem;">
       <div class="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="images/image8.png" class="d-block w-100" alt="Slide 2" style="height: 18rem;">
+      <img src="images/image8.png" class="d-block w-100" alt="Slide 2" style="height: 25rem;">
       <div class="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="images/image3.jpg" class="d-block w-100" alt="Slide 3" style="height: 18rem;">
+      <img src="images/image3.jpg" class="d-block w-100" alt="Slide 3" style="height: 25rem;">
       <div class="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
