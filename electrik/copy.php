a4bf042658fa5c3992c00ef15ae511c60f762c84
jrm947 <jrm94@njit.edu>
2023-11-30 15:23:19 -0500

    fix 1.14

--- electrik/copy.php
+++ electrik/copy.php
@@ -16,22 +16,6 @@
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner" id="carouselInner">
     <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Content-Type: application/json"); 
-
-    error_log(print_r($_REQUEST, true));
-
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-        exit(0);
-    }
 
     $json_data = file_get_contents('php://input');
 
