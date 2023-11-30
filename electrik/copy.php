4d38997ebcda9a8775f81c2a946afb5615089238
jrm947 <jrm94@njit.edu>
2023-11-30 15:17:37 -0500

    fix 1.13

--- electrik/copy.php
+++ electrik/copy.php
@@ -15,73 +15,61 @@
 <!-- Bootstrap Carousel -->
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner" id="carouselInner">
-        <?php
-        // Allow from any origin
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: *");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-        header("Content-Type: application/json"); // Set content type to JSON
+    <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Content-Type: application/json"); 
 
-        // Add this line at the beginning of the file for debugging
-        error_log(print_r($_REQUEST, true));
+    error_log(print_r($_REQUEST, true));
 
-        // Access-Control headers are received during OPTIONS requests
-        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 
-            exit(0);
-        }
+        exit(0);
+    }
 
-        // Read JSON data from the request body
-        $json_data = file_get_contents('php://input');
-        echo json_encode($json_data);  // This will output the received data
+    $json_data = file_get_contents('php://input');
 
-        // Add this line for debugging
-        error_log(print_r($json_data, true));
+    error_log(print_r($json_data, true));
 
-        if (!empty($json_data)) {
-            // Decode JSON data
-            $jsonData = json_decode($json_data, true);
+    echo json_encode($json_data);  
 
-            // Check if the expected structure is present
-            if (isset($jsonData['response']['apps']) && is_array($jsonData['response']['apps'])) {
-                $apps = $jsonData['response']['apps'];
+    if (!empty($json_data)) {
+        $jsonData = json_decode($json_data, true);
 
-                // Output carousel items dynamically based on API data
-                foreach ($apps as $index => $app) {
-                    $appId = $app['appid'];
-                    $imageUrl = "https://steamcdn-a.akamaihd.net/steam/apps/{$appId}/header.jpg";
+        if (isset($jsonData['response']['apps']) && is_array($jsonData['response']['apps'])) {
+            $apps = $jsonData['response']['apps'];
 
-                    // Set active class for the first item
-                    $activeClass = ($index === 0) ? 'active' : '';
+            foreach ($apps as $index => $app) {
+                $appId = $app['appid'];
+                $imageUrl = "https://steamcdn-a.akamaihd.net/steam/apps/{$appId}/header.jpg";
 
-                    // Output carousel item
-                    echo '<div class="carousel-item ' . $activeClass . '">';
-                    echo '<img src="' . $imageUrl . '" class="d-block w-100" alt="Card ' . $appId . '" style="height: 25rem;">';
-                    echo '</div>';
-                }
+                $activeClass = ($index === 0) ? 'active' : '';
 
-                // Print a success message to the console
-                echo '<script>';
-                echo 'console.log("Data received from the server:", ' . json_encode($jsonData) . ');';
-                echo '</script>';
-            } else {
-                // Print an error message to the console
-                echo '<script>';
-                echo 'console.error("Invalid JSON data structure from the server");';
-                echo '</script>';
+                echo '<div class="carousel-item ' . $activeClass . '">';
+                echo '<img src="' . $imageUrl . '" class="d-block w-100" alt="Card ' . $appId . '" style="height: 25rem;">';
+                echo '</div>';
             }
-        } else {
-            // Print an error message to the console
+
             echo '<script>';
-            echo 'console.error("No JSON data received from the server");';
+            echo 'console.log("Data received from the server:", ' . json_encode($jsonData) . ');';
+            echo '</script>';
+        } else {
+            echo '<script>';
+            echo 'console.error("Invalid JSON data structure from the server");';
             echo '</script>';
         }
-        ?>
+    } else {
+        echo '<script>';
+        echo 'console.error("No JSON data received from the server");';
+        echo '</script>';
+    }
+    ?>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -124,11 +112,9 @@
    &copy; 2023 Electrik.com. All rights reserved. <a class="terms-link" href="terms.php">Terms of Service</a>
 </div>
 
-<!-- Bootstrap JS (optional, but required for some features) -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-<!-- Your JavaScript file -->
 <script src="upcomingGames.js"></script>
 
 </body>
