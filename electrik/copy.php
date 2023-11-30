d044cc17db39d2f06e6db4a805434519dcd329c8
jrm947 <jrm94@njit.edu>
2023-11-30 15:07:03 -0500

    fix 1.11

--- electrik/copy.php
+++ electrik/copy.php
@@ -16,10 +16,28 @@
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner" id="carouselInner">
         <?php
-        // Read JSON data from the request body
-        $json_data = $_GET['jsonData'];
-        echo json_encode($json_data);
+        // Allow from any origin
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
         
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        
+            exit(0);
+        }
+
+        // Read JSON data from the request body
+        $json_data = file_get_contents('php://input');
+        echo json_encode($json_data);  // This will output the received data
+
         if (!empty($json_data)) {
             // Decode JSON data
             $jsonData = json_decode($json_data, true);
