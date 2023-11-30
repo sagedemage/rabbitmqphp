003827905fe056eb67ef7f3c835a5a35357183a1
jrm947 <jrm94@njit.edu>
2023-11-30 16:40:22 -0500

    temp fix 10

--- electrik/upcomingGames.js
+++ electrik/upcomingGames.js
@@ -13,30 +13,31 @@
 */
 
 axios.get('upcomingGames.php')
-.then(function (response) {
-  console.log("Raw data from server:", response.data);
-
-  if (typeof response.data === 'object' && response.data !== null) {
-    axios.post('copy.php', response.data, {
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
+  .then(function (response) {
+    console.log("Raw data from server:", response.data);
+    if (typeof response.data === 'object' && response.data !== null) {
+      const dataObject = {
+        jsonData: response.data
+      };
+      console.log("Data Object:", dataObject);
+      axios.post('copy.php', dataObject, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
       .then(function (response) {
         console.log("In js before post:", response.data);
       })
       .catch(function (error) {
         console.log(error);
-      })
-      .finally(function () {
       });
-  } else {
-    console.error("Invalid response data type. Expected object.");
-  }
-})
-.catch(function (error) {
-  console.log(error);
-});
+    } else {
+      console.error("Invalid response data type. Expected object.");
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 
 
