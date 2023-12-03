e4380089466ed3efd1ed42938e2743a13da22a80
Salmaan Saeed <salsaeed999@gmail.com>
2023-12-03 16:20:39 -0500

    document the sql query file

--- sql_queries/configure_replica.sql
+++ sql_queries/configure_replica.sql
@@ -1,8 +1,10 @@
 CHANGE REPLICATION SOURCE TO
-/* SOURCE_HOST='source_server_ip', */
-SOURCE_HOST='192.168.56.101',
+/* Replace the 'source_server_ip' with the correct source VM IP address. */
+SOURCE_HOST='source_server_ip',
 SOURCE_USER='replica_admin',
 SOURCE_PASSWORD='adminPass',
 SOURCE_LOG_FILE='mysql-bin.000001',
 SOURCE_LOG_POS=4;
+
+/* Start DB Replication */
 START REPLICA;
