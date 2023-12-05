55b51ebdd98bda851580cf94e13692e17f807734
Salmaan Saeed <salsaeed999@gmail.com>
2023-12-05 01:03:29 -0500

    add back the following html tags

--- electrik/copy.php
+++ electrik/copy.php
@@ -1,4 +1,3 @@
-<!--
 <!DOCTYPE html>
 
 <html lang="en">
@@ -6,7 +5,7 @@
 	<title>Page Title</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	< Bootstrap CSS >
+	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="style.css">
 </head>
@@ -14,7 +13,6 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
--->
 
 <!--
 <script src="upcomingGames.js"></script>
