8c69609e522d191f39237b65ac23767c4bbd81b0
hk442 <70672085+hk442@users.noreply.github.com>
2023-12-07 19:21:37 -0500

    Update deploy_cluster.sh

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -5,8 +5,8 @@ BASE_VM="BaseVM"
 VM_FRONTEND="FrontEndNode"
 VM_BACKEND="BackEndNode"
 VM_DMZ="DMZNode"
-HOSTONLY_IF="vboxnet0"
-HOSTONLY_IP_BASE="192.168.56."
+NATNETWORK_NAME="NatNetwork"
+INTERNAL_NETWORK_NAME="IntNet"
 
 # Check if VirtualBox is installed
 if ! command -v VBoxManage &> /dev/null; then
@@ -14,14 +14,13 @@ if ! command -v VBoxManage &> /dev/null; then
     exit 1
 fi
 
-# Create host-only network (if not exists)
-VBoxManage list hostonlyifs | grep -q "$HOSTONLY_IF" || VBoxManage hostonlyif create
-VBoxManage hostonlyif ipconfig "$HOSTONLY_IF" --ip "${HOSTONLY_IP_BASE}1"
+# Create NAT Network (if not exists)
+VBoxManage list natnetworks | grep -q "$NATNETWORK_NAME" || VBoxManage natnetwork add --netname "$NATNETWORK_NAME" --enable --dhcp on
 
 # Function to clone and configure VMs
 clone_and_setup_vm() {
     local vm_name="$1"
-    local vm_ip="${HOSTONLY_IP_BASE}$2"
+    local network_type="$2"
 
     # Clone the base VM
     VBoxManage clonevm "$BASE_VM" --name "$vm_name" --register
@@ -30,8 +29,14 @@ clone_and_setup_vm() {
     # VBoxManage modifyvm "$vm_name" --memory 2048 --cpus 2
 
     # Setup network
-    VBoxManage modifyvm "$vm_name" --hostonlyadapter1 "$HOSTONLY_IF"
-    VBoxManage modifyvm "$vm_name" --nic1 hostonly
+    if [ "$network_type" == "dmz" ]; then
+        # DMZ Node - Internal Network only
+        VBoxManage modifyvm "$vm_name" --nic1 intnet --intnet1 "$INTERNAL_NETWORK_NAME"
+    else
+        # Front-end and Back-end Nodes - NAT Network and Internal Network
+        VBoxManage modifyvm "$vm_name" --nic1 natnetwork --nat-network1 "$NATNETWORK_NAME"
+        VBoxManage modifyvm "$vm_name" --nic2 intnet --intnet2 "$INTERNAL_NETWORK_NAME"
+    fi
 
     # Start VM
     VBoxManage startvm "$vm_name" --type headless
@@ -43,13 +48,13 @@ clone_and_setup_vm() {
 }
 
 # Deploy Front-end Node
-clone_and_setup_vm "$VM_FRONTEND" "101" &
+clone_and_setup_vm "$VM_FRONTEND" "external" &
 
 # Deploy Back-end Node
-clone_and_setup_vm "$VM_BACKEND" "102" &
+clone_and_setup_vm "$VM_BACKEND" "external" &
 
 # Deploy DMZ Node
-clone_and_setup_vm "$VM_DMZ" "103" &
+clone_and_setup_vm "$VM_DMZ" "dmz" &
 
 wait
 echo "Cluster deployment completed."
