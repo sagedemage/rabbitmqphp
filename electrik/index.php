31a5a46c986d5917b8e74733a869a932a6552c66
jrm947 <jrm94@njit.edu>
2023-12-18 04:22:17 -0500

    further rolls

--- electrik/index.php
+++ electrik/index.php
@@ -4,13 +4,9 @@
     <title>Page Title</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS from CDN for faster loading -->
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        /* Inline critical CSS */
-        /* Add essential styles here */
-    </style>
-    <link rel="stylesheet" href="style.css"> <!-- Non-critical CSS -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
@@ -33,46 +29,33 @@
     </div>
 </form>
 
-<!-- PHP Code for Pagination -->
-<?php
-    // Define default page and limit
-    $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-    $limit = 5; // Reduced the limit for faster loading
-    $offset = ($page - 1) * $limit;
-
-    // Total items (assuming you know it's 10,000)
-    $totalItems = 100; 
-    $totalPages = ceil($totalItems / $limit);
-
-    // Generate the API request with pagination
-    $request = array();
-    $request['type'] = "GetAppList";
-    $request['offset'] = $offset; // Add offset to the request
-    $request['limit'] = $limit;   // Add limit to the request
-
-    // Sending the API request and handling the response
-    $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-    $response = $client->send_request($request);
-?>
 
 <!-- Bootstrap Carousel -->
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner" id="carouselInner">
         <?php
+        ini_set('display_errors', 1);
+
+        require_once('../rabbitmq_lib/path.inc');
+        require_once('../rabbitmq_lib/get_host_info.inc');
+        require_once('../rabbitmq_lib/rabbitMQLib.inc');
+
+        $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+        $request = array();
+        $request['type'] = "GetAppList";
+        $response = $client->send_request($request);
+
         if (is_string($response)) {
             $jsonResponse = json_decode($response);
             if (json_last_error() === JSON_ERROR_NONE && isset($jsonResponse->response->apps)) {
-                $firstItem = true;
-                foreach ($jsonResponse->response->apps as $app) {
+                foreach ($jsonResponse->response->apps as $index => $app) {
                     $appId = $app->appid;
                     $gameName = $app->name;
                     $imageUrl = "https://steamcdn-a.akamaihd.net/steam/apps/{$appId}/header.jpg";
-                    $activeClass = $firstItem ? 'active' : '';
-                    $firstItem = false;
-
+                    $activeClass = ($index === 0) ? 'active' : '';
                     echo '<div class="carousel-item ' . $activeClass . '">';
                     echo '<a href="review.php?appid=' . $appId . '&name=' . urlencode($gameName) . '">';
-                    echo '<img src="' . $imageUrl . '" class="d-block w-100 lazy" alt="' . $gameName . '" style="height: 25rem;">';
+                    echo '<img src="' . $imageUrl . '" class="d-block w-100" alt="' . $gameName . '" style="height: 25rem;">';
                     echo '</a>';
                     echo '</div>';
                 }
@@ -84,6 +67,54 @@
             echo 'console.error("Response is not a string: ", ' . json_encode($response) . ');';
             echo '</script>';
         }
+
+        // Handling for SteamID submission or clearing
+        $ownedGames = [];
+        $randomGames = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['submitSteamID']) && !empty($_POST['steamID'])) {
+                $steamID = $_POST['steamID'];
+
+                // Fetch user's most-played games using the submitted SteamID
+                $request = array();
+                $request['type'] = "GetOwnedGames";
+                $request['steamID'] = $steamID;
+                $response = $client->send_request($request);
+
+                if (is_string($response)) {
+                    $jsonResponse = json_decode($response, true);
+                    if (json_last_error() === JSON_ERROR_NONE && isset($jsonResponse['response']['games'])) {
+                        // Sort games by playtime and get top 5
+                        usort($jsonResponse['response']['games'], function($a, $b) {
+                            return $b['playtime_forever'] - $a['playtime_forever'];
+                        });
+                        $ownedGames = array_slice($jsonResponse['response']['games'], 0, 5);
+                    }
+                }
+            } else if (isset($_POST['clearSteamID'])) {
+                // Reset and show random games
+                shuffle($jsonResponse->response->apps);
+                $randomGames = array_slice($jsonResponse->response->apps, 0, 5);
+            }
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['gameSearch'])) {
+            $gameSearch = strtolower($_GET['gameSearch']);
+            $foundGame = false;
+        
+            foreach ($jsonResponse->response->apps as $app) {
+                if (strtolower($app->name) == $gameSearch) {
+                    header("Location: review.php?appid={$app->appid}&name=" . urlencode($app->name));
+                    $foundGame = true;
+                    break;
+                }
+            }
+        
+            if (!$foundGame) {
+                echo '<p>Game not found. Please try another search.</p>';
+            }
+        }
+
         ?>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -104,7 +135,7 @@
         foreach ($gamesToShow as $game): ?>
             <div class="card">
                 <a href="review.php?appid=<?php echo $game['appid']; ?>&name=<?php echo urlencode($game['name']); ?>">
-                    <img src="https://steamcdn-a.akamaihd.net/steam/apps/<?php echo $game['appid']; ?>/header.jpg" class="card-img-top lazy" alt="<?php echo $game['name']; ?>" style="height: 18rem;">
+                    <img src="https://steamcdn-a.akamaihd.net/steam/apps/<?php echo $game['appid']; ?>/header.jpg" class="card-img-top" alt="<?php echo $game['name']; ?>" style="height: 18rem;">
                 </a>
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $game['name']; ?></h5>
@@ -116,19 +147,6 @@
         <?php endforeach; ?>
     </div>
 </div>
-
-<!-- Pagination Controls -->
-<nav aria-label="Page navigation" class="mt-4">
-    <ul class="pagination justify-content-center">
-        <li class="page-item <?= ($page <= 1) ? 'disabled' : '' ?>">
-            <a class="page-link" href="?page=<?= max(1, $page - 1) ?>">Previous</a>
-        </li>
-        <!-- Example: Add a few page number links here if needed -->
-        <li class="page-item <?= ($page >= $totalPages) ? 'disabled' : '' ?>">
-            <a class="page-link" href="?page=<?= min($totalPages, $page + 1) ?>">Next</a>
-        </li>
-    </ul>
-</nav>
 
 <!-- Table of Owned Games -->
 <div class="container mt-5">
@@ -168,35 +186,6 @@
 </div>
 
 <!-- Bootstrap JS (optional, but required for some features) -->
-<!-- Load JS asynchronously for performance -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" async></script>
-<script>
-    // Lazy loading for images
-    document.addEventListener("DOMContentLoaded", function() {
-        var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-
-        if ("IntersectionObserver" in window) {
-            let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-                entries.forEach(function(entry) {
-                    if (entry.isIntersecting) {
-                        let lazyImage = entry.target;
-                        lazyImage.src = lazyImage.dataset.src;
-                        lazyImage.classList.remove("lazy");
-                        lazyImageObserver.unobserve(lazyImage);
-                    }
-                });
-            });
-
-            lazyImages.forEach(function(lazyImage) {
-                lazyImageObserver.observe(lazyImage);
-            });
-        } else {
-            // Fallback for browsers without IntersectionObserver support
-            lazyImages.forEach(function(lazyImage) {
-                lazyImage.src = lazyImage.dataset.src;
-            });
-        }
-    });
-</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
