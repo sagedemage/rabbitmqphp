3404ccb01d96a26ada49cc77326bd3997cd873b3
jrm947 <jrm94@njit.edu>
2023-12-18 01:53:17 -0500

    another change

--- electrik/index.php
+++ electrik/index.php
@@ -29,11 +29,37 @@
     </div>
 </form>
 
+<!-- PHP Code for Pagination -->
+<?php
+    // Define default page and limit
+    $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+    $limit = 10; // Or 50, as per your requirement
+    $offset = ($page - 1) * $limit;
+
+    // Total items (assuming you know it's 10,000)
+    $totalItems = 10000; 
+    $totalPages = ceil($totalItems / $limit);
+
+    // Generate the API request with pagination
+    $request = array();
+    $request['type'] = "GetAppList";
+    $request['offset'] = $offset; // Add offset to the request
+    $request['limit'] = $limit;   // Add limit to the request
+    // ... rest of your API call logic
+
+    // Sending the API request and handling the response
+    $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+    $response = $client->send_request($request);
+    // Handle the API response here...
+?>
+
 
 <!-- Bootstrap Carousel -->
+<!--
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner" id="carouselInner">
         <?php
+        /*
         ini_set('display_errors', 1);
 
         require_once('../rabbitmq_lib/path.inc');
@@ -114,7 +140,49 @@
                 echo '<p>Game not found. Please try another search.</p>';
             }
         }
+        */
+        ?>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+-->
 
+<!-- Bootstrap Carousel -->
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner" id="carouselInner">
+        <?php
+        if (is_string($response)) {
+            $jsonResponse = json_decode($response);
+            if (json_last_error() === JSON_ERROR_NONE && isset($jsonResponse->response->apps)) {
+                $firstItem = true;
+                foreach ($jsonResponse->response->apps as $app) {
+                    $appId = $app->appid;
+                    $gameName = $app->name;
+                    $imageUrl = "https://steamcdn-a.akamaihd.net/steam/apps/{$appId}/header.jpg";
+                    $activeClass = $firstItem ? 'active' : '';
+                    $firstItem = false; // Ensure only the first item is marked active
+                    
+                    echo '<div class="carousel-item ' . $activeClass . '">';
+                    echo '<a href="review.php?appid=' . $appId . '&name=' . urlencode($gameName) . '">';
+                    echo '<img src="' . $imageUrl . '" class="d-block w-100" alt="' . $gameName . '" style="height: 25rem;">';
+                    echo '</a>';
+                    echo '</div>';
+                }
+            } else {
+                echo 'Response is not a valid JSON string.';
+            }
+        } else {
+            echo '<script>';
+            echo 'console.error("Response is not a string: ", ' . json_encode($response) . ');';
+            echo '</script>';
+        }
         ?>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -147,6 +215,19 @@
         <?php endforeach; ?>
     </div>
 </div>
+
+<!-- Pagination Controls -->
+<nav aria-label="Page navigation" class="mt-4">
+    <ul class="pagination justify-content-center">
+        <li class="page-item <?= ($page <= 1) ? 'disabled' : '' ?>">
+            <a class="page-link" href="?page=<?= max(1, $page - 1) ?>">Previous</a>
+        </li>
+        <!-- Example: Add a few page number links here if needed -->
+        <li class="page-item <?= ($page >= $totalPages) ? 'disabled' : '' ?>">
+            <a class="page-link" href="?page=<?= min($totalPages, $page + 1) ?>">Next</a>
+        </li>
+    </ul>
+</nav>
 
 <!-- Table of Owned Games -->
 <div class="container mt-5">
