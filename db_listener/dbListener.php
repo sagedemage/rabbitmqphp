81ea8105bddd7610bb2fc6be01c11a9ea16a8228
Salmaan Saeed <salsaeed999@gmail.com>
2023-12-20 20:20:13 -0500

    added back the for each loop

--- db_listener/dbListener.php
+++ db_listener/dbListener.php
@@ -148,6 +148,34 @@ function getReviews($appId) {
         $result = $stmt->get_result();
         $reviews = $result->fetch_all(MYSQLI_ASSOC);
 
+		/* Set username for each review */
+		foreach ($reviews as $key => $review) {
+			$userId = $review['userId'];
+
+			$userRequest = "SELECT username FROM Users WHERE id = ?";
+			$userStmt = $db->prepare($userRequest);
+
+			if (!$userStmt) {
+				echo "Prepare failed: (" . $db->errno . ") " . $db->error . "<br>";
+				continue;
+			}
+
+			$userStmt->bind_param("i", $userId);
+			if ($userStmt->execute()) {
+				$userResult = $userStmt->get_result();
+				if ($userRow = $userResult->fetch_assoc()) {
+					$reviews[$key]['userName'] = $userRow['username'];
+				} else {
+					$reviews[$key]['userName'] = 'Unknown User';
+					echo "No user found for userID: " . $userId . "<br>";
+				}
+				$userStmt->close();
+			} else {
+				echo "Execute failed: (" . $userStmt->errno . ") " . $userStmt->error . "<br>";
+				$reviews[$key]['userName'] = 'Unknown User';
+			}
+		}
+
         $stmt->close();
         $db->close();
         return json_encode($reviews);
