b2f094b8033eb7816f079144106e3ecf346fda36
hk442 <70672085+hk442@users.noreply.github.com>
2023-12-20 22:14:59 -0500

    Create setup_systemd.sh

---
+++ setup_systemd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Replace this with your actual PHP script path
+PHP_SCRIPT_PATH="~/rabbitmqphp/db_listener/db_listener.php"
+
+# Service file content
+SERVICE_FILE_CONTENT="[Unit]
+Description=My PHP Service
+
+[Service]
+ExecStart=/usr/bin/php $PHP_SCRIPT_PATH
+Restart=always
+
+[Install]
+WantedBy=multi-user.target"
+
+# Creating systemd service file
+SERVICE_FILE="/etc/systemd/system/electrik.service"
+echo "$SERVICE_FILE_CONTENT" | sudo tee $SERVICE_FILE
+
+# Reloading systemd daemon and enabling service
+sudo systemctl daemon-reload
+sudo systemctl enable electrik.service
+
+# Starting the service
+sudo systemctl start electrik.service
+
+# Displaying the status of the service
+echo "Service status:"
+sudo systemctl status electrik.service
