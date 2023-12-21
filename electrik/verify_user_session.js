dda40462893ddd868fa4ce6a6d5638ffe3e3c091
jrm947 <jrm94@njit.edu>
2023-12-21 08:17:01 -0500

    fix logger.php

--- electrik/verify_user_session.js
+++ electrik/verify_user_session.js
@@ -1,23 +1,33 @@
 function get_cookie_value(cookie_name) {
-	let decodedCookie = decodeURIComponent(document.cookie);
-	const cookieValue = decodedCookie
-		.split("; ")
-		.find((row) => row.startsWith(cookie_name + "="))
-		?.split("=")[1];
-	return cookieValue;
+    let decodedCookie = decodeURIComponent(document.cookie);
+    const cookieValue = decodedCookie
+        .split("; ")
+        .find((row) => row.startsWith(cookie_name + "="))
+        ?.split("=")[1];
+    return cookieValue;
+}
+
+function logMessage(message) {
+    axios.post('/log_message.php', {
+        message: message
+    })
+    .catch(function(error) {
+        console.error("Logging error: ", error);
+    });
 }
 
 let user_id = get_cookie_value("user_id");
 
 axios.post('/verify_user_session.php', {
-	user_id: user_id,
+    user_id: user_id,
 })
-	.then(function(response) {
-		if (response.data !== true) {
-			location.href = "/login.php";
-		}
-	})
-	.catch(function(error) {
-		console.log(error);
-	});
-
+.then(function(response) {
+    if (response.data !== true) {
+        logMessage("Session verification failed for user_id: " + user_id);
+        location.href = "/login.php";
+    }
+})
+.catch(function(error) {
+    console.log(error);
+    logMessage("Error in session verification: " + error.toString());
+});
