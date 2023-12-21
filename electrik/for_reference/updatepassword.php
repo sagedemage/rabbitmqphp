18b9ff2a73f12875af72188f16079d063457d1d2
jrm947 <jrm94@njit.edu>
2023-12-21 08:13:53 -0500

    fix logger.php

--- electrik/for_reference/updatepassword.php
+++ electrik/for_reference/updatepassword.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', 1); 
-require_once('path/to/logger.php');
+require_once('logger.php');
 
 $logMessages = "";
 
--- electrik/validateLogin.php
+++ electrik/validateLogin.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 
-require_once('path/to/logger.php');
+require_once('logger.php');
 require_once('../rabbitmq_lib/path.inc');
 require_once('../rabbitmq_lib/get_host_info.inc');
 require_once('../rabbitmq_lib/rabbitMQLib.inc');
