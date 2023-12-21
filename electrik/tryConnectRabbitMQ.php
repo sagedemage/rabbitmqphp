0de7d6ca0765b9db98ecc9f409191478c560d556
jrm947 <jrm94@njit.edu>
2023-12-21 11:37:11 -0500

    added timeout for rabbitmq connections

--- electrik/tryConnectRabbitMQ.php
+++ electrik/tryConnectRabbitMQ.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+require_once('../rabbitmq_lib/path.inc');
+require_once('../rabbitmq_lib/get_host_info.inc');
+require_once('../rabbitmq_lib/rabbitMQLib.inc');
+
 function tryConnectRabbitMQ($primaryServer, $secondaryServer, $timeoutSeconds) {
     $startTime = time();
     while (time() - $startTime < $timeoutSeconds) {
