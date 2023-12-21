f8402497cb6be2069efdfb599b5c265a2b7f10d6
jrm947 <jrm94@njit.edu>
2023-12-20 23:56:03 -0500

    making images static on index

--- electrik/index.php
+++ electrik/index.php
@@ -13,7 +13,7 @@
 <?php include('navbar.php'); ?>
 
 <div class="container mt-5">
-   <h1 class="text-center display-4 fw-bold text-primary">Game Recommendation Portal</h1>
+   <h1 class="text-center display-4 fw-bold text-primary">Review Games on Steam</h1>
 </div>
 
 <!-- Bootstrap Carousel -->
@@ -32,44 +32,26 @@ $request['type'] = "GetAppList";
 $response = $client->send_request($request);
 
 if (is_string($response)) {
-	$jsonResponse = json_decode($response);
-	if (json_last_error() === JSON_ERROR_NONE && isset($jsonResponse->response->apps)) {
-		foreach ($jsonResponse->response->apps as $index => $app) {
-			$appId = $app->appid;
-			$gameName = $app->name;
-			$imageUrl = "https://steamcdn-a.akamaihd.net/steam/apps/{$appId}/header.jpg";
-			$activeClass = ($index === 0) ? 'active' : '';
-			echo '<div class="carousel-item ' . $activeClass . '">';
-			echo '<a href="review.php?appid=' . $appId . '&name=' . urlencode($gameName) . '">';
-			echo '<img src="' . $imageUrl . '" class="d-block w-100" alt="' . $gameName . '" style="height: 25rem;">';
-			echo '</a>';
-			echo '</div>';
-		}
-	} else {
-		echo 'Response is not a valid JSON string.';
-	}
+    $jsonResponse = json_decode($response);
+    if (json_last_error() === JSON_ERROR_NONE && isset($jsonResponse->response->apps)) {
+        foreach ($jsonResponse->response->apps as $index => $app) {
+            $appId = $app->appid;
+            $gameName = $app->name;
+            $imageUrl = "https://steamcdn-a.akamaihd.net/steam/apps/{$appId}/header.jpg";
+            $activeClass = ($index === 0) ? 'active' : '';
+            echo '<div class="carousel-item ' . $activeClass . '">';
+            echo '<img src="' . $imageUrl . '" class="d-block w-100" alt="' . $gameName . '" style="height: 25rem;">';
+            echo '</div>';
+        }
+    } else {
+        echo 'Response is not a valid JSON string.';
+    }
 } else {
-	echo '<script>';
-	echo 'console.error("Response is not a string: ", ' . json_encode($response) . ');';
-	echo '</script>';
+    echo '<script>';
+    echo 'console.error("Response is not a string: ", ' . json_encode($response) . ');';
+    echo '</script>';
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['gameSearch'])) {
-	$gameSearch = strtolower($_GET['gameSearch']);
-	$foundGame = false;
-
-	foreach ($jsonResponse->response->apps as $app) {
-		if (strtolower($app->name) == $gameSearch) {
-			header("Location: review.php?appid={$app->appid}&name=" . urlencode($app->name));
-			$foundGame = true;
-			break;
-		}
-	}
-
-	if (!$foundGame) {
-		echo '<p>Game not found. Please try another search.</p>';
-	}
-}
 
 ?>
 	</div>
