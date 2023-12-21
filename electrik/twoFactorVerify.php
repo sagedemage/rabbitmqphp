eb3848b9a5512671899230d18725c4ee0eaf7243
jrm947 <jrm94@njit.edu>
2023-12-21 05:48:19 -0500

    update twoFactorVerify

--- electrik/twoFactorVerify.php
+++ electrik/twoFactorVerify.php
@@ -14,20 +14,20 @@
     <?php include('navbar.php'); ?>
 
     <?php
-    // twoFactorVerify.php
     session_start();
-    require_once('path/to/db_connection_file.php'); // Include your DB connection file
+    require_once('path/to/db_connection_file.php'); // Update with the correct path
 
-    // Check if the user ID is passed
-    if (!isset($_GET['user_id'])) {
+    $errorMsg = '';
+
+    // Check if the user ID is passed and valid
+    if (!isset($_GET['user_id']) || empty($_GET['user_id'])) {
         header('Location: login.php');
         exit;
     }
 
     $userId = $_GET['user_id'];
-    $errorMsg = '';
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['twoFactorCode'])) {
         $userCode = $_POST['twoFactorCode'];
 
         // Fetch the stored code and expiry from the database
@@ -36,38 +36,46 @@
         $stmt->bind_param("s", $userId);
         $stmt->execute();
         $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
+        if ($row = $result->fetch_assoc()) {
+            $currentDateTime = new DateTime();
+            $expiryDateTime = new DateTime($row['code_expiry']);
 
-        $currentDateTime = new DateTime();
+            if ($userCode === $row['two_factor_code'] && $currentDateTime < $expiryDateTime) {
+                // Code is correct and not expired, set cookie and redirect to dashboard
+                $name = "user_id";
+                $value = $userId; // Consider encrypting this value
+                $expires_or_options = time() + 3600; // 1 hour
+                $path = "/";
+                $secure = false; // Set to true if using HTTPS
+                $http_only = true; // Set to true to make cookie accessible only through the HTTP protocol
 
-        if ($row && $userCode === $row['two_factor_code'] && $currentDateTime < new DateTime($row['code_expiry'])) {
-            // Code is correct and not expired, set cookie and redirect to dashboard
-            $name = "user_id";
-            $value = $userId; // Consider encrypting this value
-            $expires_or_options = time() + 3600; // 1 hour
-            $path = "/";
-            $secure = false; // Set to true if using HTTPS
-            $http_only = true; // Set to true to make cookie accessible only through the HTTP protocol
+                setcookie($name, $value, $expires_or_options, $path, "", $secure, $http_only);
 
-            setcookie($name, $value, $expires_or_options, $path, "", $secure, $http_only);
-
-            header("Location: dashboard.php");
-            exit;
+                header("Location: dashboard.php");
+                exit;
+            } else {
+                $errorMsg = 'Invalid or expired code. Please try again.';
+            }
         } else {
-            $errorMsg = 'Invalid or expired code. Please try again.';
+            $errorMsg = 'Error fetching user data. Please try again.';
         }
+        $stmt->close();
     }
     ?>
 
-    <h2>Enter your 2FA Code</h2>
-    <form action="twoFactorVerify.php?user_id=<?php echo htmlspecialchars($userId); ?>" method="post">
-        <label for="twoFactorCode">2FA Code:</label>
-        <input type="text" id="twoFactorCode" name="twoFactorCode" required>
-        <button type="submit">Verify</button>
-    </form>
-    <?php if ($errorMsg): ?>
-        <p><?php echo htmlspecialchars($errorMsg); ?></p>
-    <?php endif; ?>
+    <div class="container">
+        <h2>Enter your 2FA Code</h2>
+        <form action="twoFactorVerify.php?user_id=<?php echo htmlspecialchars($userId); ?>" method="post">
+            <label for="twoFactorCode">2FA Code:</label>
+            <input type="text" id="twoFactorCode" name="twoFactorCode" required>
+            <button type="submit" class="btn btn-primary">Verify</button>
+        </form>
+        <?php if (!empty($errorMsg)): ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo htmlspecialchars($errorMsg); ?>
+            </div>
+        <?php endif; ?>
+    </div>
 
     <?php include('footer.php'); ?>
 
